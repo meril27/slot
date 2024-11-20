c5661f3dfec9ec7651671f938395d2d8674e59f1
meril27 <merilgoldlina2k@gmail.com>
2024-11-20 20:41:35 +0530

    Success

--- timetable2/timetable2/asgi.py
+++ timetable2/timetable2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for timetable project.
+ASGI config for timetable2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timetable.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timetable2.settings')
 
 application = get_asgi_application()
